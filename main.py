a4e3cb6b3ba483a76a29f6aa8e31a24c2ccc5c14
Kim Huynh <kimr843@gmail.com>
2022-10-26 16:46:31 -0400

    lab6

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ def encoder(password):
         result += int(i) - 2
         result = str(result)
     return result
+
+def decoder(password):
+    decode = ''
+
+    for i in password:
+        decode += str(int(i) - 3)
+    return decode
+
